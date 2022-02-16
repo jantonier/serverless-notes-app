951477674287a6478a00f90335d89720819a4786
jantonier <97867182+jantonier@users.noreply.github.com>
2022-02-16 04:09:06 -0500

    Securing the API with AWS Cognito

--- src/create.js
+++ src/create.js
@@ -3,20 +3,20 @@ import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
 export const main = handler(async (event) => {
-  const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    Item: {
-      // The attributes of the item to be created
-      userId: "123", // The id of the author
-      noteId: uuid.v1(), // A unique uuid
-      content: data.content, // Parsed from request body
-      attachment: data.attachment, // Parsed from request body
-      createdAt: Date.now(), // Current Unix timestamp
-    },
-  };
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+            // The attributes of the item to be created
+            userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
+            noteId: uuid.v1(), // A unique uuid
+            content: data.content, // Parsed from request body
+            attachment: data.attachment, // Parsed from request body
+            createdAt: Date.now(), // Current Unix timestamp
+        },
+    };
 
-  await dynamoDb.put(params);
+    await dynamoDb.put(params);
 
-  return params.Item;
+    return params.Item;
 });
